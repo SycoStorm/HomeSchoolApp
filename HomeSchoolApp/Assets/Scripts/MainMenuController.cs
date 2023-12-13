0e31855aabfe418c9b86a4ab924cf043751ef663
Destiney0 <desfirefly@gmail.com>
2023-12-13 10:20:06 -0500

    MainMenu Scene, text mesh pro

---
+++ HomeSchoolApp/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public GameObject credentialsPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+}
